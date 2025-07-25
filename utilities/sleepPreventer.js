cd3828358190b3004688b99ca021360f9df9cfcc
s-r-e-e-r-a-g <sreekuttankottakkal@gmail.com>
2025-07-25 21:46:10 +0530

    Update sleepPreventer.js

--- utilities/sleepPreventer.js
+++ utilities/sleepPreventer.js
@@ -5,6 +5,7 @@ cron.schedule('*/10 * * * *', async () => {
   try {
     const response = await axios.get(`https://cinewish-app.onrender.com/ping`);
     console.log(`Server: ${response.data.message}`);
+    await axios.get(`https://turfgrid.onrender.com/`);
   } catch (error) {
     console.error('Error calling /ping:', error.message);
   }
