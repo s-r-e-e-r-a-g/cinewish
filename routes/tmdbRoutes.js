7c862273817120e43122eefcf0d48f2e394691a3
s-r-e-e-r-a-g <sreekuttankottakkal@gmail.com>
2025-07-28 22:38:37 +0530

    Update tmdbRoutes.js

--- routes/tmdbRoutes.js
+++ routes/tmdbRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getDetails, getRecommendations, getTrending, getUpcoming, searchTMDB } from "../controllers/tmdbController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ router.get("/trending", getTrending);
 router.get("/upcoming", getUpcoming);
 router.get("/search", searchTMDB);
 router.get("/details/:media/:id", getDetails);
-router.get("/recommendations/:media/:id", getRecommendations);
+router.get("/recommendations/:media/:id", authMiddleware, getRecommendations);
 
 export default router;
