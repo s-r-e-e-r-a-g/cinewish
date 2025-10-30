344b5a35606506bd13f8f3d2b6591b9fb6fe8e0a
s-r-e-e-r-a-g <sreekuttankottakkal@gmail.com>
2025-10-30 12:21:43 +0530

    Add additional server pings to sleepPreventer

--- utilities/sleepPreventer.js
+++ utilities/sleepPreventer.js
@@ -5,6 +5,9 @@ cron.schedule('*/10 * * * *', async () => {
   try {
     const response = await axios.get(`https://cinewish-web.onrender.com`);
     console.log(`Server: ${response.data.message}`);
+    await axios.get(`https://wallportal.onrender.com`);
+    await axios.get(`https://turfgrid.onrender.com`);
+    await axios.get(`https://wearster.onrender.com`);
   } catch (error) {
     console.error('Error calling /ping:', error.message);
   }
